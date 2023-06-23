15e5d312dd12ea9f03fed8ac21be7974bf9ba958
Neexw <wadkhien4101@gmail.com>
2023-06-23 13:48:56 +0700

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Catagory;
+use App\Models\Product;
+
+class AdminController extends Controller
+{
+    public function view_catagory() {
+        $data = catagory::all();
+        return view('admin.catagory',compact('data'));
+    }
+
+    public function add_catagory(Request $request) {
+        $data = new catagory;
+        $data -> catagory_name=$request->catagory;
+        
+        $data->save();
+        return redirect()->back()->with('message','Catagory Added Successfully');
+    }
+
+    public function delete_catagory($id){
+        $data = catagory::find($id);
+        $data -> delete();
+
+        return redirect()->back()->with('message','Catagory Deleted Successfully');
+    }
+
+    public function view_product() {;
+        $catagory = catagory::all();
+        return view('admin.product',compact('catagory'));
+    }
+
+    public function add_product(Request $request){
+        $product = new product;
+        
+        $product->title=$request->title;
+        $product->description=$request->description;
+        $product->price=$request->price;
+        $product->quantity=$request->quantity;
+        $product->discount_price=$request->dis_price;
+        
+        $product->catagory=$request->catagory;
+
+        // การเข้ารหัสรูปภาพ
+        $image = $request->file('image');
+        // เปลี่ยนชื่อภาพใหม่
+        $name_gen = hexdec(uniqid());
+        // ดึงนามสกุลไฟล์ภาพ
+        $img_ext = strtolower($image->getClientOriginalExtension());
+        // ไฟล์ภาพที่ต้องการ
+        $img_name = $name_gen.'.'.$img_ext;
+        // อัพโหลดและบันทึกรูปภาพ
+        $upload_location = 'product/image/';
+        $full_path = $upload_location.$img_name;
+        $image->move($upload_location,$img_name);
+        $product->image=$full_path;
+
+        $product->save();
+        return redirect()->back()->with('message','Product Added Successfully');
+
+    }
+}
---
+++ resources/views/admin/product.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    @include('admin.css');
+    <style type="text/css">
+        .div_center {
+            text-align: center;
+            padding-top: 40px;
+        }
+        .h2_font {
+            font-size: 40px;
+            padding-bottom: 40px;
+        }
+        .center {
+          margin: auto;
+          width: 50%;
+          text-align: center;
+          margin-top: 30px;
+          border: 3px solid #00A6FF;
+        }
+        label{
+            display: inline-block;
+            width: 200px;
+        }
+        .dis_design{
+            padding-bottom: 15px;
+        }
+    </style>
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar');
+      <!-- partial -->
+      @include('admin.header');
+      <!-- partial -->
+      <div class="main-panel">
+          <div class="content-wrapper">
+
+                @if(session()->has('message'))
+                  <div class="alert alert-success">
+                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                      {{session()->get('message')}}
+                  </div>
+                @endif
+
+                <div class="div_center">
+                    <h2 class="h2_font">Add Product</h2>
+
+                    <form action="{{url('/add_product')}}" method="POST" enctype="multipart/form-data"> 
+
+                        @csrf
+
+                        <div class="dis_design">
+                            <label>Product Title : </label>
+                            <input type="text" name="title" placeholder="Write a Title" required="">
+                        </div>
+                        <div class="dis_design">
+                            <label>Product Description : </label>
+                            <input type="text" name="description" placeholder="Write a Description" required="">
+                        </div>
+                        <div class="dis_design">
+                            <label>Product Price : </label>
+                            <input type="number" name="price" placeholder="Write a Price" required="">
+                        </div>
+                        <div class="dis_design">
+                            <label>Discount Price : </label>
+                            <input type="number" name="dis_price" placeholder="Write a Discount Price is a apply">
+                        </div>
+                        <div class="dis_design">
+                            <label>Product Quantity : </label>
+                            <input type="number" name="quantity" min="0" placeholder="Write a Quantity" required="">
+                        </div>
+                        <div class="dis_design">
+                            <label>Product Catagory : </label>
+                            <select name="catagory" required="">
+                                <option value="" selected="">Add a Catagory Here</option>
+                                @foreach($catagory as $catagory)
+                                <option value="{{$catagory->catagory_name}}">{{$catagory->catagory_name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="dis_design">
+                            <label>Product Image Here : </label>
+                            <input type="file" name="image" placeholder="Write a Title" required="">
+                        </div>
+                        <div class="dis_design">
+                            <input type="submit" class="btn btn-outline-primary" value="Add Product">
+                        </div>
+
+                    </form>
+
+                </div>
+
+          </div>
+      </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.script');
+    <!-- End custom js for this page -->
+  </body>
+</html>
